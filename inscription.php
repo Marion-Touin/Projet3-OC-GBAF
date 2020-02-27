a81309d436da1427a7a28badf30c8508efc3bf6e
marion touin <t.mation@hotmail.com>
2020-02-27 08:02:32 +0100

    projet

---
+++ inscription.php
@@ -0,0 +1,90 @@
+<?php
+// connexion à la base de données
+require_once('inc/connec.php');
+
+if(isset($_POST['inscription'])) 
+{
+	$nom = htmlspecialchars($_POST['nom']);
+	$prenom = htmlspecialchars($_POST['prenom']);
+   	$username = htmlspecialchars($_POST['username']);
+   	$password = sha1($_POST['password']);
+   	$question = $_POST['question'];
+   	$reponse = htmlspecialchars($_POST['reponse']);
+   if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['username']) AND !empty($_POST['password']) AND !empty($_POST['question']) AND !empty($_POST['reponse'])) 
+   	{
+      $usernamelength = strlen($username);
+      if($usernamelength <= 255) 
+      {
+        $requsername = $bdd->prepare("SELECT * FROM membres WHERE username = ?");
+        $requsername->execute(array($username));
+        $usernameexist = $requsername->rowCount();
+        if($usernameexist == 0) 
+        {
+          $insertmbr = $bdd->prepare("INSERT INTO membres(nom, prenom, username, password, question, reponse) VALUES(?, ?, ?, ?, ?, ?)");
+          $insertmbr->execute(array($nom, $prenom, $username, $password, $question, $reponse));
+          $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
+        } 
+	  }
+}
+}
+?>
+<html>
+   <head>
+      <title>TUTO PHP</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="style_css/inscription.css">
+   </head>
+   <body>
+    <img class="alignCenter" src="image/logo-gbaf">
+      <div align="center" class="form">
+         <form method="POST" action="">
+          
+           <h2> Formulaire d'inscription</h2>
+		<div >
+			<label class="form-label" for="nom"> Nom </label> <br/>
+			<input class="form-input" type="text"  placehorder="nom" name="nom" id="nom" value="<?php if(isset($nom)) { echo $nom; }?>"> 
+		</div>
+
+		<div>
+			<label class="form-label" for="prenom"> Prenom </label> <br/>
+			<input class="form-input" type="text" placehorder="prenom" name="prenom" id="prenom" value="<?php if(isset($prenom)) { echo $prenom; }?>">
+		</div>
+
+		<div>
+			<label class="form-label" for="username">	Pseudonyme </label> <br/>
+			<input class="form-input" type="text" placehorder="username" name="username" id="username" value="<?php if(isset($username)) { echo $username; }?>">
+		</div>
+
+		<div>
+			<label class="form-label" for="password">	Mot de passe </label> <br/>
+			<input class="form-input" type="password" placehorder="password" name="password" id="password"> 
+		</div>
+
+    <div>
+      <label class="form-label" for="question" > Question secrète <br/>
+        <select name="question" class="form-label">
+        <option value="1"> Quel est le nom de votre mère ? </option>
+        <option value="2"> Quel est la destination de vos rêves ? </option>
+        <option value="3"> Quel est le métier de votre père ? </option>
+      </label>
+      </select>
+    </div>
+
+		<div>
+			<label class="form-label" for="reponse"> Réponse question secrète</label> <br/> 
+			<input class="form-input" type="text" placehorder="reponse" name="reponse" id="reponse"> 
+		</div>
+
+		<div>
+			<input type="submit" name="inscription" value="Valider"> 
+		</div>
+      <p class="para"> Si vous possédez déjà un compte,connectez-vous <a class="para" href="connexion.php">ICI</a>! </p>
+         </form>
+         <?php
+         if(isset($erreur)) {
+            echo '<font color="red">'.$erreur."</font>";
+         }
+         ?>
+      </div>
+   </body>
+</html>
