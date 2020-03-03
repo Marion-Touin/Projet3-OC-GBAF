5992a6efa5f9780a5bb742c9cf18f26e4a1641b6
xallia <61024700+xallia@users.noreply.github.com>
2020-03-03 14:32:17 +0100

    Projet fini

--- connexion.php
+++ connexion.php
@@ -1,60 +1,82 @@
 <?php
-session_start();
-
 // connexion à la base de données
 require_once('inc/connec.php');
 
-if(isset($_POST['formconnexion'])) {
-   $usernameconnect = htmlspecialchars($_POST['username']);
-   $passwordconnect = sha1($_POST['password']);
-   if(!empty($usernameconnect) AND !empty($passwordconnect)) {
-      $requser = $bdd->prepare("SELECT * FROM membres WHERE username = ? AND password = ?");
-      $requser->execute(array($usernameconnect, $passwordconnect));
-      $userexist = $requser->rowCount();
-      if($userexist == 1) {
-         $userinfo = $requser->fetch();
-         $_SESSION['id'] = $userinfo['id'];
-         $_SESSION['nom'] = $userinfo['nom'];
-         $_SESSION['prenom'] = $userinfo['prenom'];
-         header("Location: accueil.php?id=".$_SESSION['id']);
-      } else {
-         $erreur = "Mauvais mail ou mot de passe !";
-      }
-   } else {
-      $erreur = "Tous les champs doivent être complétés !";
-   }
+// envoie du formulaire
+if(isset($_POST['formconnexion'])) 
+{
+  // empêcher d'écrite de l'html
+  $username = htmlspecialchars($_POST['username']);
+  $password = htmlspecialchars($_POST['password']);
+
+  //vérification que les champs soient remplis
+  if(!empty($username) AND !empty($password)) 
+  {    
+    $requser = $bdd->prepare("SELECT * FROM membres WHERE username = :username");
+    $requser->execute(array(
+      'username' => $_POST['username']));
+      $userinfo = $requser->fetch();
+
+      // vérification du mot de passe
+      $passwordcorrect = password_verify($_POST['password'], $userinfo['password']);
+
+        if($passwordcorrect)
+        {
+          session_start();
+          $_SESSION['id'] = $userinfo['id'];
+          $_SESSION['nom'] = $userinfo['nom'];
+          $_SESSION['prenom'] = $userinfo['prenom'];
+          header("Location: accueil.php?id=".$_SESSION['id']);
+        } 
+        else 
+        {
+          $erreur = "Mauvais pseudo ou mot de passe !";
+        }    
+   }  
+  else 
+  {
+    $erreur = "Tous les champs doivent être complétés !";
+  }
 }
 ?>
 <html>
-   <head>
-      <title>TUTO PHP</title>
-      <meta charset="utf-8">
-      <link rel="stylesheet" href="style_css/connexion.css">
-   </head>
-   <body>
-    <img class="alignCenter" src="image/logo-gbaf">
-      <div align="center">
-         <form class="form" method="post" action="">
-           <h2> Connexion</h2>
+  <head>
+    <title>Connexion</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style_css/connex.css">
+  </head>
+  <body>
+    <!-- Logo gbaf -->
+    <img class="logo" src="image/logo-gbaf" alt="Logo gbaf">
+
+      <div>
+        <!-- Formulaire -->
+        <form class="form" method="post" action="">
+        <h1> Connexion</h1>
+        
         <div>
-           <label class="form-label" for="username">  Pseudonyme </label> <br/>
-           <input class="form-input" type="text" placehorder="username" name="username" > </br> </br>
+          <label class="form-label" for="username">  Pseudonyme </label> <br/>
+          <input class="form-input" type="text" placehorder="username" name="username" > </br> </br>
         </div>
+
         <div>
-            <label class="form-label" for="password"> Mot de passe </label> <br/>
-            <input class="form-input" type="password" placehorder="password" name="password" > </br> </br>
+          <label class="form-label" for="password"> Mot de passe </label> <br/>
+          <input class="form-input" type="password" placehorder="password" name="password" > </br> </br>
         </div>
+
         <div>
-             <input type="submit" name="formconnexion" value="Se connecter !">
+          <input type="submit" name="formconnexion" value="Se connecter !">
         </div>
-        <p> Si vous  ne possédez pas de compte, veuillez vous inscrire <a href="inscription.php">ICI</a>! </p>
-        <a class="mdp" href="mdpoublie.php"> Mot De Passe Oublié </a>
-         </form>
-         <?php
+
+          <p> Si vous  ne possédez pas de compte, veuillez vous inscrire <a href="inscription.php">ICI</a>! </p>
+          <a class="mdp" href="mdpoublie.php"> Mot De Passe Oublié </a>
+        </form>
+        <?php
          if(isset($erreur)) {
             echo '<font color="red">'.$erreur."</font>";
          }
          ?>
       </div>
+<?php require_once('inc/footer.php'); ?>
    </body>
 </html>
