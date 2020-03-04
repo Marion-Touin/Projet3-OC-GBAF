ad5dcfad22223ceb75069db31ad95d81e41470ba
xallia <61024700+xallia@users.noreply.github.com>
2020-03-04 13:46:00 +0100

    projet fini

--- accueil.php
+++ accueil.php
@@ -1,7 +1,6 @@
 <?php
 	// connexion à la base de données
     require_once('inc/connec.php');
-    // récupération du nom et prénom
     require_once('inc/header.php'); 
 ?>
 <!DOCTYPE html>
@@ -15,7 +14,7 @@
 	<!-- présentation de gbaf -->
 	<h1> Le Groupement Banque Assurance Français </h1><br/>
 		<p class="gbaf">
-			Le Groupement Banque Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français : </br></p>
+			Le Groupement Banque Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français :</br></p>
 				<ul class="gbaf">
 					<li> BNP Paribas </li>
 					<li> BPCE </li>
@@ -28,46 +27,61 @@
 		Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l’échelle nationale.
 		C’est aussi un interlocuteur privilégié des pouvoirs publics.
 		</p>
-	<br/><br/><br/>
 
-	<!-- partenaires -->
+	<img class="illustration" src="image/illustration.png">
+
 	<h2>Partenaires</h2>
-
 	<!-- partenaire formation_co -->
-		<div>	
-			<img class="alignLeft"; src="image/formation_co">
+	<div class="conteneur">	
+		<div class="logo">
+			<img src="image/formation_co.png" alt="logo formation&co">
+		</div>
+		<div>
 			<h3>Formation & Co</h3>
 			<p>Formation&co est une association française présente sur tout le territoire...</p>
-			<button class="alignRight";> <a href="partenaires.php?id=1">lire la suite</a></button>
+			<button class="bouton"> <a href="partenaires.php?id=1">lire la suite</a></button>
 		</div>
+	</div>
 		<br/>
 
 		<!-- partenaire protectpeople -->
-		<div>	
-			<img class="alignLeft"; src="image/protectpeople">
+	<div class="conteneur">
+		<div class="logo clear">	
+			<img  src="image/protectpeople.png" alt="logo protectpeople">
+		</div>
+		<div>
 			<h3>Protectpeople</h3>
 			<p>Protectpeople finance la solidarité nationale... </p>
-			<button class="alignRight";> <a href="partenaires.php?id=2">lire la suite</a></button>
+			<button class="bouton" > <a href="partenaires.php?id=2">lire la suite</a></button>
 		</div>
+	</div>	
 		<br/>
 
-				<!--partenaire Dsa_france -->
-		<div>	
-			<img class="alignLeft"; src="image/Dsa_france">
+		<!--partenaire Dsa_france -->
+	<div class="conteneur">
+		<div class=" logo clear">	
+			<img  src="image/Dsa_france.png" alt="logo Dsa_france">
+		</div>
+		<div>
 			<h3>Dsa France</h3>
 			<p>Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales... </p>
-			<button class="alignRight";> <a href="partenaires.php?id=3">lire la suite</a></button>
+			<button class="bouton"> <a href="partenaires.php?id=3">lire la suite</a></button>
 		</div>
+	</div>
 		<br/>
 
-
 		<!-- partenaire CDE -->
-		<div>	
-			<img class="alignLeft"; src="image/CDE">
-			<h3>CDE</h3>
-			<p>La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation... </p>
-			<button class="alignRight";> <a href="partenaires.php?id=4">lire la suite</a></button>
+	<div class="conteneur">
+		<div class=" logo clear">	
+			<img src="image/CDE.png" alt="logo CDE">
 		</div>
-
+		<div>
+			<h3>Chambre Des Entrepreneurs</h3>
+			<p>La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation... </p>
+			<button class="bouton"> <a href="partenaires.php?id=4">lire la suite</a></button> 
+		</div>
+	</div> 
+	<br/>
+<?php require_once('inc/footer.php'); ?>
 </body>
 </html>
